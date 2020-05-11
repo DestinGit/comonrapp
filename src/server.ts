b62044f7d46675dcf88bfd7ab74c78cf0beff4d2
Destin <d.gandobak@gmail.com>
2020-05-11 18:27:15 +0200

    Reorganiser le code + Implémentation de server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,41 @@
-import { Request, Response } from "express";
+//Définition des modules
 import express from "express";
+import { Request, Response } from "express";
+
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 export default class Server {
 
     readonly port: number;
+    // private _app: any;
 
     constructor (port: number) {
         this.port = port;
+        // this._app = express();
     }
 
     start () {
         const app = express();
-        app.get('/', (req: Request, res: Response) => res.send('Salut les gens !'));
+        const urlEncodedParser = bodyParser.urlencoded({
+            extended: true
+        });
 
-        app.listen(this.port, () => console.log('Serveur Démarré'))
+        app.use(urlEncodedParser);
+        app.use(bodyParser.json());
+
+        //Définition des CORS
+        app.use((req:Request, res:Response, next) => {
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            res.setHeader('Access-Control-Allow-Credentials', 1);
+            next();
+        });
+
+        //On définit la route Hello
+        app.get('/hello', (req: Request, res: Response) => res.send('Salut les gens !'));
+
+        app.listen(this.port, () => console.log(`Listening on port ${this.port}`))
     }
 }
