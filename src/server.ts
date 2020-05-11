198f70e266720f112acb983d8fd0785974e39017
Destin <d.gandobak@gmail.com>
2020-05-11 15:14:16 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+const express = require('express');
+
+export default class Server {
+
+    readonly port: number;
+
+    constructor (port: number) {
+        this.port = port;
+    }
+
+    start () {
+        const app = express();
+        app.get('/', (req: Request, res: Response) => res.send('Salut les gens !'));
+
+        app.listen(this.port, () => console.log('Serveur Démarré'))
+    }
+}
